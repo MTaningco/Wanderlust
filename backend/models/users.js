3e26e557102f9389d2cf5c7a4458c146401e8064
Miguel <jmigst@gmail.com>
2021-01-11 14:50:21 -0800

    Implement users model.

---
+++ backend/models/users.js
@@ -0,0 +1,46 @@
+var pool = require('./db');
+var bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+
+var Users = {
+  createUser : async function(email, password, username, callback){
+    // Hash the email
+    bcrypt.hash(email, saltRounds, async function(err, hashedEmail) {
+      // Hash the password
+      bcrypt.hash(password, saltRounds, async function(err, hashedPassword) {
+        // console.log(hashedEmail, hashedPassword);
+        try{
+          const queryResult = await pool.query(`
+            insert into AppUsers(email, password, username) 
+            values ($1, $2, $3) 
+            returning * 
+          `, [hashedEmail, hashedPassword, username]);
+          callback(queryResult.rows[0]);
+        }
+        catch(err){
+          callback(null);
+        }
+      });
+    });
+  },
+
+  getUser : async function(username, password, callback){
+    const dbPassword = await pool.query(`
+      select user_uid, username, password 
+      from AppUsers 
+      where username = $1
+    `, [username]);
+    const data = dbPassword.rows;
+    if(data.length === 1){
+      bcrypt.compare(password, data[0].password, function(err, result) {
+        callback(result, data[0]);
+      });
+    }
+    else{
+      callback(null);
+    }
+  }
+}
+
+module.exports = Users;
