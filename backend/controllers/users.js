36e71a29f49c1f1cdf2420f8b1cffe4f37a505fb
Miguel <jmigst@gmail.com>
2021-01-11 14:51:08 -0800

    Implement users controllers and routes.

---
+++ backend/controllers/users.js
@@ -0,0 +1,36 @@
+var users = require('../models/users');
+var jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'default';
+
+exports.login = function(req, res, next){
+  users.getUser(req.body.username, req.body.password, (result, data) => {
+    if(result){
+      jwt.sign({ user_uid: data.user_uid, username: data.username }, JWT_SECRET_KEY, {expiresIn: "1h"}, function(err, token) {
+        if(err){
+          res.status(501).send("error at get user");
+        }
+        else{
+          res.json({token});
+        }
+      });
+    }
+    else{
+      res.status(401).send('Unauthorized');
+    }
+  })
+}; 
+
+exports.createUser = function(req, res, next){
+  users.createUser(req.body.email, req.body.password, req.body.username, (rows) => {
+    if(rows){
+      res.json(rows);
+    }
+    else{
+      res.status(409).send("Failed to make a new user");
+    }
+  });
+}
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,16 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const pool = require('../models/db');
+var users = require('../controllers/users');
 
-/* GET users listing. */
-router.get('/', async (req, res) => {
-  try{
-    const allUsers = await pool.query("select * from appusers");
-    res.json(allUsers.rows);
-  }
-  catch(err){
-    console.error(err.message);
-  }
-});
+//post to make a new account
+router.post('/create', users.createUser);
+
+//post to check if the account is properly authenticated
+router.post('/login', users.login);
 
 module.exports = router;
