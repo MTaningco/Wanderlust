81535754e261b3e9d6c92fbc58848a48887e5dd0
Miguel <jmigst@gmail.com>
2021-01-13 16:54:51 -0800

    Add production code to db.js.

--- backend/models/db.js
+++ backend/models/db.js
@@ -9,12 +9,18 @@ const DB_NAME = process.env.DB_NAME || 'default';
 const DB_HOST = process.env.DB_HOST || 'default';
 const DB_PORT = process.env.DB_PORT || 'default';
 
-const pool = new Pool({
+const devConfig = {
   user: DB_USER,
   password: DB_PASS,
   host: DB_HOST,
   port:DB_PORT,
   database: DB_NAME 
-});
+};
+
+const prodConfig = {
+  connectionString : process.env.DATABASE_URL || 'default'
+}
+
+const pool = new Pool(process.env.NODE_ENV === "production" ? prodConfig : devConfig);
 
 module.exports = pool;
