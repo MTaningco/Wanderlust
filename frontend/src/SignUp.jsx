2794f88b082712c958dbd6b5205624e1d46e6f10
Miguel <jmigst@gmail.com>
2021-01-25 19:49:25 -0800

    Clean up SignUp.

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -4,23 +4,41 @@ import React, { Component, useState, useRef, useEffect } from "react";
 import {  Redirect, Link } from "react-router-dom";
 
 function SignUp() {
-
+  //States
   // const [isTokenValid, setIsTokenValid] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [isUsernameTaken, setIsUsernameTaken] = useState(false);
 
+  /**
+   * Checks if a string has a number.
+   * @param {String} value - the string used to check if it has a number
+   */
   const hasNumber = value => {
     return new RegExp(/[0-9]/).test(value);
   }
+
+  /**
+   * Checks if a string has mixed caps.
+   * @param {String} value - the string used to check if it has mixed caps
+   */
   const hasMixed = value => {
     return new RegExp(/[a-z]/).test(value) && new RegExp(/[A-Z]/).test(value);
   }
+
+  /**
+   * Checks if a string has special characters
+   * @param {*} value - the string used to check if it has special characters
+   */
   const hasSpecial = value => {
     return new RegExp(/[!#@$%^&*)(+=._-]/).test(value);
   }
 
+  /**
+   * Gets the human readable value of a password strength.
+   * @param {number} count - the password strength
+   */
   const strengthMagnitude = count => {
     if (count < 2)
        return 'Very Weak';
@@ -34,6 +52,10 @@ function SignUp() {
        return 'Strong';
   }
 
+  /**
+   * Gets the strength of a password.
+   * @param {String} value - the string used to check the strenght of a password
+   */
   const strengthIndicator = value => {
     let strengths = 0;
     if (value.length > 5)
@@ -49,19 +71,35 @@ function SignUp() {
     return strengthMagnitude(strengths);
   }
 
+  /**
+   * Handles the username change.
+   * @param {*} event - the event for the username change
+   */
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
     getUsernameStatus(event.target.value);
   };
 
+  /**
+   * Handles the password change.
+   * @param {*} event - the event for the password change
+   */
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
 
+  /**
+   * Handles the email change.
+   * @param {*} event - the event for the email change
+   */
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
   };
 
+  /**
+   * Gets the availability of the username
+   * @param {String} value - the string used to check the availability of a username
+   */
   const getUsernameStatus = (value) => {
     fetch(`/users/exists?username=${value}`)
     .then(res => res.json())
@@ -70,6 +108,9 @@ function SignUp() {
     })
   }
 
+  /**
+   * Handles the user signing up.
+   */
   const handleSignUp = () => {
     if(email === "" || password === "" || username === ""){
       alert("At least one field is empty!");
@@ -80,16 +121,6 @@ function SignUp() {
         "password" : password,
         "username" : username
       }
-  
-      // console.log("body used for signing in", body);
-      // const temp = {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json"
-      //   },
-      //   body: JSON.stringify(body)
-      // };
-      // console.log("parameters used with the api", temp);
   
       fetch(`/users/create`, {
         method: "POST",
