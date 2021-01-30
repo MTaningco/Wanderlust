dec3ca93b6fe9ec55ebec24251a9bedb945c1582
Miguel <jmigst@gmail.com>
2021-01-30 01:55:33 -0800

    Improve UI of LoggedOut.

--- frontend/src/LoggedOut.jsx
+++ frontend/src/LoggedOut.jsx
@@ -1,5 +1,7 @@
 //Imports from libraries
 import React, { Component, useState, useRef, useEffect } from "react";
+import AppBar from '@material-ui/core/AppBar';
+import { Typography, Toolbar } from "@material-ui/core";
 import {
   Link
 } from "react-router-dom";
@@ -8,10 +10,17 @@ function LoggedOut() {
 
     return (
         <div>
-            <br/>
-            Session has expired. Please sign back in.
-            <br/>
-            <Link to="/">Back to Sign In</Link>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6">
+                        Wanderlust
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Typography variant="h4" style={{ padding: 60 }}>
+                Session has expired. Please sign back in.
+            </Typography>
+            <Link to="/" style={{ padding: 60 }}>Back to Sign In</Link>
         </div>
     );
 }
