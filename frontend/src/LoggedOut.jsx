64aca254a1d564ecf6317e94925d2aba9af99bad
Miguel <jmigst@gmail.com>
2021-02-04 12:10:33 -0800

    Update styles of LoggedOut.

--- frontend/src/LoggedOut.jsx
+++ frontend/src/LoggedOut.jsx
@@ -1,26 +1,45 @@
 //Imports from libraries
 import React, { Component, useState, useRef, useEffect } from "react";
-import AppBar from '@material-ui/core/AppBar';
-import { Typography, Toolbar } from "@material-ui/core";
+// import AppBar from '@material-ui/core/AppBar';
+import { Typography, Toolbar, Grid } from "@material-ui/core";
 import {
   Link
 } from "react-router-dom";
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import ScheduleIcon from '@material-ui/icons/Schedule';
+
+const useStyles = makeStyles((theme) => ({
+    link: {
+        padding: 60, 
+        color: "#7096ff", 
+        textDecoration: "none", 
+        fontSize: "17px", 
+        fontWeight: "bold"
+    },
+    timerIcon: {
+        width: "30%",
+        height: "30%"
+    },
+}));
 
 function LoggedOut() {
+    const classes = useStyles();
 
     return (
         <div>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6">
-                        Wanderlust
+            <Grid container 
+                    spacing={0}
+                    align="center"
+                    justify="center"
+                    direction="column" style={{ height:"100vh", flexGrow: 1}}>
+                <Grid item>
+                    <ScheduleIcon className={classes.timerIcon}/>
+                    <Typography variant="h4" style={{ padding: 60 }}>
+                        Session has expired. Please sign back in.
                     </Typography>
-                </Toolbar>
-            </AppBar>
-            <Typography variant="h4" style={{ padding: 60 }}>
-                Session has expired. Please sign back in.
-            </Typography>
-            <Link to="/" style={{ padding: 60 }}>Back to Sign In</Link>
+                    <Link to="/" className={classes.link}>Back to Sign In</Link>
+                </Grid>
+            </Grid>
         </div>
     );
 }
