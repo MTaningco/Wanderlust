52ef8922814625c01954a7b80c73a7367fc97550
Miguel <jmigst@gmail.com>
2021-02-09 15:57:09 -0800

    Change icon for viewing paths.

--- frontend/src/EditPathsTab.jsx
+++ frontend/src/EditPathsTab.jsx
@@ -6,8 +6,6 @@ import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import MyLocationIcon from '@material-ui/icons/MyLocation';
-// import InfoIcon from '@material-ui/icons/Info';
-import VisibilityIcon from '@material-ui/icons/Visibility';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
 import AirplanemodeActiveIcon from '@material-ui/icons/AirplanemodeActive';
@@ -344,7 +342,7 @@ function EditPathsTab({value, index, invalidateAuth, setEditPath, updateLandmark
           return(
             <Paper style={{marginTop: "10px", marginBottom: "10px", padding:"10px"}}  elevation={2}>
               <IconButton>
-                {isProcessing ? <CircularProgress style={{color: "white"}}size={24} color='secondary' disableShrink /> : <VisibilityIcon />}
+                {isProcessing ? <CircularProgress style={{color: "white"}}size={24} color='secondary' disableShrink /> : <MyLocationIcon />}
               </IconButton>
               <IconButton color="primary" onClick={() => handleEditPathMode(element)}>
                 {isProcessing ? <CircularProgress size={24} color='primary' disableShrink /> : <EditIcon />}
