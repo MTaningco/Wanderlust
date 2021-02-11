457e31958b345aa7d1161c6f48cc6d458ccc6592
Miguel <jmigst@gmail.com>
2021-02-10 22:26:37 -0800

    Document the controllers in the backend.

--- backend/controllers/tokenHeader.js
+++ backend/controllers/tokenHeader.js
@@ -1,3 +1,9 @@
+/**
+ * Verifies that a token exists.
+ * @param {*} req - the request body, containing the authorization header
+ * @param {*} res - the result body
+ * @param {*} next - the next function to execute
+ */
 exports.verifyToken = function(req, res, next){
   const bearerHeader = req.headers['authorization'];
   if(typeof bearerHeader !== undefined){
